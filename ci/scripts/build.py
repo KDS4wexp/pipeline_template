b07b8a065beb921040b3be13627923aaeeb6374d
Dmitry <KDS4wexp@gmail.com>
2024-12-22 20:46:16 +0300

    build script

--- ci/scripts/build.py
+++ ci/scripts/build.py
@@ -12,6 +12,6 @@ subprocess.run([sys.executable, "-m", 'pip', 'install', "conan"], check=True)
 build_type = ["Release", "Debug"]
 
 for type in build_type:
-  logger.info("Starting build ${type}")
-  subprocess.run(['conan', 'build', '.', '-s', 'build_type=${type}', '-c', 'tools.build:skip_test=False', '-pr:a=./ci/src/conan-profile/profile-release-gcc-13-x86_64', '--build="*"'], check=True)
+  logger.info(f"Starting build {type}")
+  subprocess.run(['conan', 'build', '.', '-s', f'build_type={type}', '-c', 'tools.build:skip_test=False', '-pr:a=./ci/src/conan-profile/profile-release-gcc-13-x86_64', '--build="*"'], check=True)
 

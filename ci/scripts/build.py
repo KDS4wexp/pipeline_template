28242eea053e5c45e54416e79e0d02e37a82676e
KDS4wexp <KDS4wexp@gmail.com>
2024-12-24 00:23:36 +0300

    pipeline

--- ci/scripts/build.py
+++ ci/scripts/build.py
@@ -1,17 +1,18 @@
-import os
-import sys
-import subprocess
-import logging
+# import os
+# import sys
+# import subprocess
+# import logging
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG)
 
-logger.info("Installing conan")
-subprocess.run([sys.executable, '-m', 'pip', 'install', 'conan'], check=True)
+# logger.info("Installing conan")
+# subprocess.run([sys.executable, '-m', 'pip', 'install', 'conan'], check=True)
 
-build_type = ["Release", "Debug"]
+# build_type = ["Release", "Debug"]
 
-for type in build_type:
-  logger.info(f"Starting build {type}")
-  subprocess.run(['conan', 'build', '.', '-s', f'build_type={type}', '-c', 'tools.build:skip_test=False', '-pr:a=./ci/src/conan-profile/profile-release-gcc-13-x86_64', '--build="*"'], check=True)
+# for type in build_type:
+#   logger.info(f"Starting build {type}")
+#   subprocess.run(['conan', 'build', '.', '-s', f'build_type={type}', '-c', 'tools.build:skip_test=False', '-pr:a=./ci/src/conan-profile/profile-release-gcc-13-x86_64', '--build="*"'], check=True)
 
+print("build")
